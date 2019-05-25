3f91034edd8acc6a661b7721af20b6df1a4f81e3
ri7nz <ri7nz.labs@gmail.com>
2019-05-25 15:34:20 +0700

    add: setup test for  project.

---
+++ frontend/tests/index.test.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+
+import { shallow } from "enzyme";
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "../pages/index.js";
+
+describe("With Enzyme", () => {
+  it('App shows "Hello world!"', () => {
+    const app = shallow(<App />);
+    const assertText =
+      "We love those people—and you will too—because they shares so many great " +
+      "tutorials for Web Developer in Bahasa. Help us to reach more people if " +
+      "your favorite ones is not listed here yet.";
+
+    expect(app.find("p").text()).toEqual(assertText);
+  });
+});
+
+describe("With Snapshot Testing", () => {
+  it('App shows "Hello world!"', () => {
+    const component = renderer.create(<App />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
