b7e7223ffecc448265b92c8f20b65aa40b3ddb12
ri7nz <ri7nz.labs@gmail.com>
2019-05-25 15:33:56 +0700

    add: test setup for root directory.

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env:{
+        test: {
+            presets: [
+                [
+                    "@babel/preset-env",
+                    {
+                        targets: {
+                            node: "current"
+                        }
+                    }
+                ],
+                "@babel/preset-react",
+                "babel-preset-next",
+            ]
+
+        }  
+    }
+};
